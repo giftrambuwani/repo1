533d2189f46b9896b3afa8efd6fe6c44622b98ae
giftrambuwani <47209873+giftrambuwani@users.noreply.github.com>
2025-01-29 14:10:57 +0200

    Add files via upload
    first added file

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("streamlit is amazing")
+
+st.write("Hello, Streamlit!")
+
+st.header("Number selection")
+
+number = st.slider("Pick a number", 1, 100)
+st.write(f"You picked: {number}")
